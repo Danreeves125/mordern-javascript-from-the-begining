7c96bce78ac2d9af98bb0f840b63617dd40d8261
Dan Reeves <danreeves125@hotmail.com>
2020-05-21 12:46:15 +0100

    getrepos built, github finder complete

--- section-8-api-projects/github-finder/github.js
+++ section-8-api-projects/github-finder/github.js
@@ -2,15 +2,23 @@ class Github {
     constructor() {
         this.client_id = 'c66dfc282bbd6e3e86d4';
         this.client_secret = 'f837fad2488d07b6181bcc0e2ee22cfd4563e6ab';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profileData = await profileResponse.json();
+        const repoData = await repoResponse.json();
 
         return {
-            profile: profileData
+            profile: profileData,
+            repos: repoData
         }
     }
+
+
 }
--- section-8-api-projects/github-finder/ui.js
+++ section-8-api-projects/github-finder/ui.js
@@ -3,6 +3,7 @@ class UI {
         this.profile = document.getElementById('profile');
     }
 
+    // display profile
     showProfile(user) {
         this.profile.innerHTML = `
             <div class="card card-body mb-3">
@@ -30,5 +31,67 @@ class UI {
             <h3 class="page-heading mb-3">Latest Repos</h3>
             <div id="repos"></div>
         `
+    }
+
+    // Show User Repo
+    showRepos(repos) {
+        let output = '';
+
+        repos.forEach((repo) => {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+
+        // Out Repos
+        document.getElementById('repos').innerHTML = output;
+    }
+
+    // Show alert message
+    showAlert(msg, className) {
+        // clear any remaining alerts
+        this.clearAlert();
+        // Create di
+        const div = document.createElement('div');
+        // Add Classes
+        div.className = className;
+        // add text
+        div.appendChild(document.createTextNode(msg));
+        // Get Parent
+         const container = document.querySelector('.searchContainer');
+        // Get search box
+        const search = document.querySelector('.search');
+        // Insert Alert
+        container.insertBefore(div, search);
+
+        // Timeout after 3 seconds
+        setTimeout(() => {
+            this.clearAlert();
+        }, 2000);
+    }
+
+    // Clear Alert Message
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    // clear profile
+    clearProfile() {
+        this.profile.innerHTML = '';
     }
 }
